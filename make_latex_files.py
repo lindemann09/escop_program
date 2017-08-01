1ef125664b57999106d31e0f8756ab59794a8084
Oliver Lindemann <lindemann09@users.noreply.github.com>
2017-08-01 11:44:03 +0200

    Update make_latex_files.py

--- make_latex_files.py
+++ make_latex_files.py
@@ -1,14 +1,7 @@
-# poster session have no end time
+from program_maker import structure, latex_convert
 
-
-from program_maker import structure,  latex_convert
-
-
-filename="contribution_overview.incl.tex"
 sessions_csv='ESCoP2017_sessions.csv'
 authors_csv="ESCoP2017_subsumed_authors.csv"
 
 escop = structure.Conference(sessions_csv, authors_csv)
-
-latex_convert.create_overview(escop, filename)
-
+latex_convert.create_overview(escop, filename="contribution_overview.incl.tex")
